675cdfae2086c050d0e8b73f8a02e82c51b4248e
admer escobar <admerescobar@gmail.com>
2023-06-14 16:13:32 -0300

    Agregando carpeta de componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import {Typography } from  '@mui/material';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Typography>
-          HELLO PEOPLE
-        </Typography>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
     </div>
   );
 }
---
+++ src/components/main/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const  main = () => {
+  return (
+<div>index</div>  )
+}
+
+export default main;
